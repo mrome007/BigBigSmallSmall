cffc82caf5e96c0fcca9df6c2e4d302230c31521
mrome007 <mrome007@ucr.edu>
2017-12-12 21:41:00 -0800

    Player movement and implement player getting bigger/smaller

---
+++ Assets/Scripts/BigSmallMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigSmallMovement : MonoBehaviour 
+{
+	public float Speed = 5f;
+	public Transform TopRightCorner;
+	public Transform BottomLeftCorner;
+
+	private Vector2 movementVector;
+	private float xMax;
+	private float xMin;
+	private float yMax;
+	private float yMin;
+
+	private void Start()
+	{
+		movementVector = Vector2.zero;
+		xMax = TopRightCorner.position.x;
+		xMin = BottomLeftCorner.position.x;
+		yMax = TopRightCorner.position.y;
+		yMin = BottomLeftCorner.position.y;
+	}
+
+	private void Update()
+	{
+		var horizontal = Input.GetAxis("Horizontal");
+		var vertical = Input.GetAxis("Vertical");
+
+		movementVector = new Vector2(horizontal, vertical);
+		transform.Translate(movementVector * Speed * Time.deltaTime);
+
+		CapMovement();
+	}
+
+	private void CapMovement()
+	{
+		var xPos = transform.position.x; 
+		xPos = Mathf.Clamp(xPos, xMin, xMax);
+		var yPos = transform.position.y; 
+		yPos = Mathf.Clamp(yPos, yMin, yMax);
+
+		transform.position = new Vector2(xPos, yPos);
+	}
+}
---
+++ Assets/Scripts/GetBigOrSmall.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetBigOrSmall : MonoBehaviour 
+{	
+	private BigSmallMovement playerMovement;
+
+	private void Awake()
+	{
+		playerMovement = GetComponent<BigSmallMovement>();
+		if(playerMovement == null)
+		{
+			Debug.LogError("No BigSmallMovement component");
+		}
+	}
+
+	private void Update()
+	{
+		BigSmallInput();
+	}
+
+	private void BigSmallInput()
+	{
+		if(Input.GetKey(KeyCode.N))
+		{
+			GetBigSmall(false);
+		}
+
+		if(Input.GetKey(KeyCode.M))
+		{
+			GetBigSmall(true);
+		}
+	}
+
+	private void GetBigSmall(bool small)
+	{
+		var scale = transform.localScale;
+		if(small)
+		{
+			if(scale.x > 1)
+			{
+				scale.x--;
+				scale.y--;
+				transform.localScale = scale;
+				playerMovement.Speed += 0.5f;
+			}
+		}
+		else
+		{
+			if(scale.x < 25)
+			{
+				scale.x++;
+				scale.y++;
+				transform.localScale = scale;
+				playerMovement.Speed -= 0.5f;
+			}
+		}
+	}
+}
